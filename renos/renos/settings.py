462aa5ba80dad74ead9de152fdf17190c7d1db92
theo <theodoulos.p@fdmgroup.com>
2015-10-28 10:35:29 +0000

    Added static and media paths

--- renos/renos/settings.py
+++ renos/renos/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -82,3 +86,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+)
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
--- renos/renos/urls.py
+++ renos/renos/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'renos.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+	# Examples:
+	# url(r'^$', 'renos.views.home', name='home'),
+	# url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls')),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^rango/', include('rango.urls')),
 )
